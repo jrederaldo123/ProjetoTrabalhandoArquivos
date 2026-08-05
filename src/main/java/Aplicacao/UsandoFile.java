dcabd7afc09abaafcf45e569aa56700bdf6eeaac
Ederaldo Muniz Barreto Junior <jrederaldo123@gmail.com>
2026-08-05 16:05:33 -0300

    Primeiro commit - Prtojeto Arquivos

---
+++ src/main/java/Aplicacao/UsandoFile.java
@@ -0,0 +1,26 @@
+package Aplicacao;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class UsandoFile {
+    public static void main(String[] args) {
+        File file = new File("c:\\temp\\in.txt");
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        } catch (IOException e) {
+                System.out.println("Erro: " + e.getMessage());
+        }  finally {
+                if (sc != null){
+                    sc.close();
+                }
+        }
+
+        }
+    }
+
---
+++ src/main/java/Aplicacao/UsandoFileWriterBufferedWriter.java
@@ -0,0 +1,8 @@
+package Aplicacao;
+
+public class UsandoFileWriterBufferedWriter {
+    public static void main(String[] args) {
+        String[] lines = new String[] {"Good morning", "Good afternoon", "Good night"};
+        String path =
+    }
+}
